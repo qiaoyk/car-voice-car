515f4024e0337375c445358ab9f3a472e57e8d3e
qiaoyk <34905478+qiaoyk@users.noreply.github.com>
2018-01-03 12:44:38 +0800

    Update avoid_ultrasonic.ino

--- avoid_ultrasonic.ino
+++ avoid_ultrasonic.ino
@@ -1,9 +1,8 @@
-
 #include <Servo.h> 
-#include <LiquidCrystal.h> //申明1602液晶的函数库
+//#include <LiquidCrystal.h> //申明1602液晶的函数库
 //申明1602液晶的引脚所连接的Arduino数字端口，8线或4线数据模式，任选其一
 //LiquidCrystal lcd(12,11,10,9,8,7,6,5,4,3,2);   //8数据口模式连线声明
-LiquidCrystal lcd(13,12,7,6,5,4,3); //4数据口模式连线声明 P13--LCD 4脚  P12--LCD 5脚  
+//LiquidCrystal lcd(13,12,7,6,5,4,3); //4数据口模式连线声明 P13--LCD 4脚  P12--LCD 5脚  
               //P7--LCD 6脚   P6--LCD 11脚  P5--LCD 12脚  P4--LCD 13脚  P3--LCD 14脚  
 
 int Echo = A1;  // Echo回声脚(P2.0)
@@ -37,7 +36,7 @@ void setup()
   //初始化超声波引脚
   pinMode(Echo, INPUT);    // 定义超声波输入脚
   pinMode(Trig, OUTPUT);   // 定义超声波输出脚
-  lcd.begin(16,2);      //初始化1602液晶工作                       模式
+//  lcd.begin(16,2);      //初始化1602液晶工作                       模式
   //定义1602液晶显示范围为2行16列字符  
 }
 //=======================智能小车的基本动作=========================
@@ -170,24 +169,24 @@ void Distance_test()   // 量出前方距离
   Distance = Fdistance;
 }  
 
-void Distance_display()//显示距离
-{
-   if((2<Distance)&(Distance<400))
-  {
-    lcd.home();        //把光标移回左上角，即从头开始输出   
-    lcd.print("    Distance: ");       //显示
-    lcd.setCursor(6,2);   //把光标定位在第2行，第6列
-    lcd.print(Distance);        //显示距离
-    lcd.print("cm");            //显示
-  }
-  else
-  {
-    lcd.home();        //把光标移回左上角，即从头开始输出  
-    lcd.print("!!! Out of range");       //显示超出距离
-  }
-  delay(250);
-  lcd.clear();
-}
+//void Distance_display()//显示距离
+//{
+  // if((2<Distance)&(Distance<400))
+  //{
+    //lcd.home();        //把光标移回左上角，即从头开始输出   
+    //lcd.print("    Distance: ");       //显示
+    //lcd.setCursor(6,2);   //把光标定位在第2行，第6列
+    //lcd.print(Distance);        //显示距离
+    //lcd.print("cm");            //显示
+  //}
+  //else
+  //{
+    //lcd.home();        //把光标移回左上角，即从头开始输出  
+   // lcd.print("!!! Out of range");       //显示超出距离
+  //}
+  //delay(250);
+//  lcd.clear();
+//}
 
 void loop()
 {
@@ -195,7 +194,7 @@ void loop()
   while(1)
   {
     Distance_test();//测量前方距离
-    Distance_display();//液晶屏显示距离
+//    Distance_display();//液晶屏显示距离
     
     if(Distance < 40)//数值为碰到障碍物的距离，可以按实际情况设置
       while(Distance < 40)//再次判断是否有障碍物，若有则转动方向后，继续判断
@@ -205,7 +204,7 @@ void loop()
         digitalWrite(beep,HIGH);          //蜂鸣器停止
        // brake(1);//停车
         Distance_test();//测量前方距离
-        Distance_display();//液晶屏显示距离
+//        Distance_display();//液晶屏显示距离
       }
     else
       
@@ -215,6 +214,8 @@ void loop()
       }
   }
 }
+
+
 
 
 
